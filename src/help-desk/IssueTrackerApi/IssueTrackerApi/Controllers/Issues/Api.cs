1436af67c76603e201fc9178b39d4db64c8712ff
Joe Curran <joseph_curran@progressive.com>
2024-04-22 16:18:27 -0400

    pushing EOD

--- src/help-desk/IssueTrackerApi/IssueTrackerApi/Controllers/Issues/Api.cs
+++ src/help-desk/IssueTrackerApi/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -1,5 +1,109 @@
-﻿namespace IssueTrackerApi.Controllers.Issues;
+﻿using FluentValidation;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
 
-public class Api
+namespace IssueTrackerApi.Controllers.Issues;
+
+public class Api(IDocumentSession session) : ControllerBase
 {
+
+    // GET /issues
+    [HttpGet("/issues")]
+    public async Task<ActionResult> GetTheIssuesAsync([FromQuery] string software = "all")
+    {
+        if (software == "all")
+        {
+            var issues = await session.Query<Issue>().ToListAsync();
+            return Ok(issues);
+        }
+        else
+        {
+            var issues = await session.Query<Issue>().Where(i => i.Software == software).ToListAsync();
+            return Ok(issues);
+        }
+    }
+
+    [HttpGet("/issues/{id:guid}")]
+    public async Task<ActionResult> GetIssueByIdAsync(Guid id)
+    {
+        var issue = await session.Query<Issue>().SingleOrDefaultAsync(i => i.Id == id);
+
+        if (issue is null)
+        {
+            return NotFound();
+
+        }
+        else
+        {
+            return Ok(issue);
+        }
+    }
+
+    [HttpPost("/issues")]
+    public async Task<ActionResult> AddIssueAsync(
+       [FromBody] CreateIssueRequestModel request,
+       [FromServices] IValidator<CreateIssueRequestModel> validator)
+    {
+        var results = await validator.ValidateAsync(request);
+        if (results.IsValid)
+        {
+            var response = new Issue
+            {
+                CreatedAt = DateTimeOffset.UtcNow,
+                Description = request.Description,
+                Software = request.Software,
+                Id = Guid.NewGuid(),
+                Status = IssueStatus.Created
+            };
+            // do our thing.
+            session.Store(response);
+            await session.SaveChangesAsync();
+            return Ok(response);
+        }
+        else
+        {
+            return BadRequest(results.ToDictionary()); // 400
+        }
+
+    }
+}
+
+public record CreateIssueRequestModel
+{
+
+    public string Software { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
+
+public record Issue
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Software { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public IssueStatus Status { get; set; }
+
+}
+public enum IssueStatus { Created }
+
+public class CreateIssueRequestModelValidator : AbstractValidator<CreateIssueRequestModel>
+{
+    private readonly IReadOnlyList<string> _supportedSoftware = ["excel", "powerpoint", "word"];
+    public CreateIssueRequestModelValidator()
+    {
+        RuleFor(i => i.Description)
+            .NotEmpty()
+            .MaximumLength(1024);
+
+        RuleFor(i => i.Software)
+            .NotEmpty()
+            .Must(i =>
+            {
+                var sw = i.ToLowerInvariant().Trim();
+                return _supportedSoftware.Any(s => s == sw);
+
+            }).WithMessage("Unsupported Software. Good Luck");
+
+    }
 }
--- src/help-desk/IssueTrackerApi/IssueTrackerApi/Program.cs
+++ src/help-desk/IssueTrackerApi/IssueTrackerApi/Program.cs
@@ -1,14 +1,30 @@
+using FluentValidation;
+using IssueTrackerApi.Controllers.Issues;
+using Marten;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var databaseConnectionString = builder.Configuration.GetConnectionString("data") ?? throw new Exception("No Connection String");
+Console.WriteLine("using the connection string " + databaseConnectionString);
+builder.Services.AddMarten(options =>
+{
+    options.Connection(databaseConnectionString);
+}).UseLightweightSessions();
 
-builder.Services.AddControllers();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateIssueRequestModelValidator>();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// the stuff above this line - configuration of the api and all the stuff inside of it.
 var app = builder.Build();
-
+// the stuff after this line. - it is how it takes requests and makes them into responses.
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -18,6 +34,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); // Use .NET Reflection to go find all the routes to create the route table.
 
-app.Run();
+app.Run(); // This is a "blocking" message pump.
