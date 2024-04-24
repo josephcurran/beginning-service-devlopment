948fbb66c2a803893871990692915b5475e8d551
Joe Curran <joseph_curran@progressive.com>
2024-04-24 09:24:29 -0400

    pushing start day

--- dev-environments/postgres/db/issues.sql
+++ dev-environments/postgres/db/issues.sql
@@ -1,1 +1,1 @@
-create database Issues
+create database issues
---
+++ src/HelpDeskSupportSolution/HelpDeskSupportApi.ContractTests/GettingSupportInfo.cs
@@ -0,0 +1,20 @@
+﻿
+using Alba;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace HelpDeskSupportApi.ContractTests;
+public class GettingSupportInfo
+{
+    [Fact]
+    public async Task GetsAnOk()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/");
+            api.StatusCodeShouldBeOk();
+        });
+    }
+}
+
